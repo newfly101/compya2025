a54f79d44d611a4f96a6451651c25800dcad76f4
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 06:17:53 +0900

    [page] 공지사항 page 추가

---
+++ web/src/pages/Notice.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notice = () => {
+  return (
+    <div>
+      <h2>공지사항</h2>
+      <p>공식/컴투스프로야구 공지, 이벤트, 쿠폰을 한 곳에서 확인하세요.</p>
+    </div>
+  );
+};
+
+export default Notice;
--- web/src/router/router.jsx
+++ web/src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "@/App.jsx";
 import Home from "@/pages/Home.jsx";
 import Login from "@/pages/Login.jsx";
+import Notice from "@/pages/Notice.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
         path: "login",
         element: <Login />,
       },
+      {path: "notice", element: <Notice />,}
     ],
   },
 ]);
