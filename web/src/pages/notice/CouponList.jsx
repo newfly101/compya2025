e459014c92c72a3110b28c1c6963fbf4c4aaecf8
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 08:00:54 +0900

    [page] coupon list 추가 => disabled button mouse style 변경해야함

--- web/src/pages/notice/CouponList.jsx
+++ web/src/pages/notice/CouponList.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import CouponCard from "@/components/common/CouponCard.jsx";
+import styles from "@/styles/layout/CouponCard.module.scss";
 
-const CouponList = () => {
+import { parseDate } from "@/utils/parseDate.js";
+import { sortCoupons } from "@/utils/sortCoupons.js";
+
+const CouponList = ({data}) => {
+  const sorted = sortCoupons(data);
+  const now = new Date();
+
   return (
-    <div>🎁 쿠폰 리스트</div>
+    <div>🎁 쿠폰 리스트
+      <div className={styles.listGrid}>
+        {sorted.map((item) => {
+          const expired = parseDate(item.expireDate) < now;
+
+          console.log(parseDate(item.expireDate), now);
+
+          return (
+            <CouponCard
+              key={item.code}
+              code={item.code}
+              rewardTitle={item.rewardTitle}
+              rewardDetail={item.rewardDetail}
+              expireDate={item.expireDate}
+              disabled={expired}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
