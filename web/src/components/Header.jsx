cc471ad87a958233d4e280052410725a3474b665
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 06:18:58 +0900

    [update] 기본 메뉴 설정

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -6,13 +6,13 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.inner}>
         <nav className={styles.nav}>
-          <Link to="/" className="logo">컴프야펀</Link>
-          <Link to="/damage">데미지 계산기</Link>
-          <Link to="/skill">스킬 계산기</Link>
+          <Link to="/" >컴프야펀</Link>
+          {/*<Link to="/damage">데미지 계산기</Link>*/}
+          {/*<Link to="/skill">스킬 계산기</Link>*/}
           <Link to="/events">이벤트</Link>
           <Link to="/coupon">쿠폰 코드</Link>
           <Link to="/notice">공지사항</Link>
-          <Link to="/community">커뮤니티</Link>
+          {/*<Link to="/community">커뮤니티</Link>*/}
         </nav>
       </div>
     </header>
