cbc37893e938cb848ace92c4d76d913b927a951c
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 07:59:46 +0900

    [data] coupon data 추가 => 추후 db로 이관

---
+++ web/src/data/Coupon.js
@@ -0,0 +1,127 @@
+const coupons = [
+  {
+    code: "CPB2025CAFE40",
+    rewardTitle: "플래티넘 코치팩 1장",
+    rewardDetail: "40만 달성 쿠폰",
+    expireDate: "2025. 05. 31 23:59"
+  },
+  {
+    code: "10thCPB1",
+    rewardTitle: "Live 구단선택 플래티넘팩 1장 + 500스타",
+    rewardDetail: "10주년 뷰잉 파티 시작 쿠폰",
+    expireDate: "2025. 05. 31 23:59"
+  },
+  {
+    code: "10thCPB5",
+    rewardTitle: "에픽선수팩 1장 + 500스타",
+    rewardDetail: "10주년 뷰잉 파티 종료 쿠폰",
+    expireDate: "2025. 05. 31 23:59"
+  },
+  {
+    code: "CPB5HAPPYMAY5",
+    rewardTitle: "Live 구단선택팩 3장 + 500스타",
+    rewardDetail: "어린이날 특별 쿠폰",
+    expireDate: "2025. 05. 31 23:59"
+  },
+  {
+    code: "CPB5PARENTS8",
+    rewardTitle: "Live 플래티넘팩 1장 + 플래티넘 복권 5개",
+    rewardDetail: "어버이날 특별 쿠폰",
+    expireDate: "2025. 05. 31 23:59"
+  },
+  {
+    code: "CPBSYGIFT05",
+    rewardTitle:
+      "고급 골드팩 2 + 고급 실버팩 2 + 고급 코치팩 2 + 플래티넘 복권 2",
+    rewardDetail: "수여니의 깜짝 쿠폰",
+    expireDate: "2025. 06. 30 23:59"
+  },
+  {
+    code: "MEMORIAL06CPB",
+    rewardTitle: "국가대표 선수팩 2장 + 300 스타",
+    rewardDetail: "현충일 특별 쿠폰",
+    expireDate: "2025. 06. 30 23:59"
+  },
+  {
+    code: "RAINYCPB06",
+    rewardTitle: "구단선택 플래티넘팩 1장 + 300 스타",
+    rewardDetail: "수여니의 깜짝 쿠폰",
+    expireDate: "2025. 07. 31 23:59"
+  },
+  {
+    code: "KBO100DAYS",
+    rewardTitle: "구단선택 골드팩 3장 + 플래티넘 복권 3개",
+    rewardDetail: "개막 100일 기념 쿠폰",
+    expireDate: "2025. 07. 31 23:59"
+  },
+  {
+    code: "2025ASTAROPEN",
+    rewardTitle: "최고급 구단 선택 골드팩 1개 + 플래티넘 장비 5개",
+    rewardDetail: "올스타전 전야제 쿠폰",
+    expireDate: "2025. 08. 31 23:59"
+  },
+  {
+    code: "2025NEWSTART",
+    rewardTitle: "Live 플래티넘팩 1개 + 500스타",
+    rewardDetail: "올스타전 특별 쿠폰",
+    expireDate: "2025. 08. 31 23:59"
+  },
+  {
+    code: "INDEPENDENCE815",
+    rewardTitle: "능력치 선택 강화권 1개 + 능력치 선택 약화권 1개",
+    rewardDetail: "광복절 쿠폰",
+    expireDate: "2025. 09. 30 23:59"
+  },
+  {
+    code: "2008KBASEBALL",
+    rewardTitle:
+      "국가대표 선수팩 2장 + 히스토리 모드 볼 3개 + 500스타",
+    rewardDetail: "야구의 날 쿠폰",
+    expireDate: "2025. 09. 30 23:59"
+  },
+  {
+    code: "9CPBFALL9",
+    rewardTitle: "구단 선택 코치팩 1장 + 포지션 선택 코치팩 1장",
+    rewardDetail: "가을 맞이 쿠폰",
+    expireDate: "2025. 10. 31 23:59"
+  },
+  {
+    code: "9CPBSPORTS23",
+    rewardTitle:
+      "마스코트&의상팩 3장 + 히스토리 모드 볼 1개 + 타이틀 홀더 볼 1개",
+    rewardDetail: "가을 운동회 쿠폰",
+    expireDate: "2025. 10. 31 23:59"
+  },
+  {
+    code: "KBOXCPB1200",
+    rewardTitle: "구단선택 플래티넘팩 1장 + 1,200 스타",
+    rewardDetail: "1200만 관중 기념 쿠폰",
+    expireDate: "2025. 10. 31 23:59"
+  },
+  {
+    code: "CPB2025CAFE42",
+    rewardTitle: "구단선택 플래티넘팩 1장",
+    rewardDetail: "카페 42만명 달성 쿠폰",
+    expireDate: "2025. 12. 31 23:59"
+  },
+  {
+    code: "KBO2025END",
+    rewardTitle: "100% 플래티넘팩 + 300스타",
+    rewardDetail: "2025 GOODBYE 쿠폰",
+    expireDate: "2025. 12. 31 23:59"
+  },
+  {
+    code: "CPBULTIMATE1120",
+    rewardTitle: "레전드 재료 선수 만능카드 1개",
+    rewardDetail: "얼티밋모드 기념 쿠폰",
+    expireDate: "2025. 12. 31 23:59"
+  },
+  {
+    code: "FRIDAYSTAR300",
+    rewardTitle: "고유능력 변경권 3개 + 300스타",
+    rewardDetail: "블랙프라이데이 쿠폰",
+    expireDate: "2025. 12. 31 23:59"
+  }
+];
+
+export default coupons;
