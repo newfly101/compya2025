0ac026f6740b24deba248e3ff127193b6833c51e
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 11:59:27 +0900

    [css] card 작은 버전 multiple design 추가

--- web/src/pages/notice/CouponList.jsx
+++ web/src/pages/notice/CouponList.jsx
@@ -5,14 +5,17 @@ import styles from "@/styles/layout/CouponCard.module.scss";
 import { parseDate } from "@/utils/parseDate.js";
 import { sortCoupons } from "@/utils/sortCoupons.js";
 
-const CouponList = ({data}) => {
+const CouponList = ({data, limit, short=false}) => {
   const sorted = sortCoupons(data);
   const now = new Date();
 
+  const finalList = limit ? sorted.slice(0, limit) : sorted;
+  console.log("finalList",finalList);
+
   return (
-    <div>🎁 쿠폰 리스트
-      <div className={styles.listGrid}>
-        {sorted.map((item) => {
+    <div>{!short && "🎁 쿠폰 리스트"}
+      <div className={`${short ? styles.shortListGrid : styles.listGrid}`}>
+        {finalList.map((item) => {
           const expired = parseDate(item.expireDate) < now;
 
           console.log(parseDate(item.expireDate), now);
@@ -25,6 +28,7 @@ const CouponList = ({data}) => {
               rewardDetail={item.rewardDetail}
               expireDate={item.expireDate}
               disabled={expired}
+              short={short}
             />
           );
         })}
