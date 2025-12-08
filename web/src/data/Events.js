766d7bc21c98cf831b5afca427a073d0613ee421
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 11:30:06 +0900

    [data] event data add

---
+++ web/src/data/Events.js
@@ -0,0 +1,34 @@
+import img1 from "@/assets/events/EventoffseasonHeat.jpg";
+import img2 from "@/assets/events/Memories Draw.jpg";
+import img3 from "@/assets/events/Spot the Difference.jpg";
+
+const events = [
+  {
+    id: 1,
+    title: "[이벤트] 2026시즌 대비 오프시즌 타격 이벤트(11/13~3/28)",
+    dateRange: "2025년 11월 13일 12:00 ~ 2026년 3월 28일 23:59",
+    startDate: "2025. 11. 13",
+    endDate: "2026. 03. 28",
+    image: img1,
+    link: "https://cafe.naver.com/com2usbaseball2015/1923858"
+  },
+  {
+    id: 2,
+    title: "[이벤트] 추억의 뽑기 이벤트(~12/31)",
+    dateRange: "2025년 12월 01일 12:00 ~ 2025년 12월 31일 23:59",
+    startDate: "2025. 12. 01 12:00",
+    endDate: "2025. 12. 31 23:59",
+    image: img2,
+    link: "https://cafe.naver.com/com2usbaseball2015/1928065"
+  },
+  {
+    id: 3,
+    title: "[이벤트] 수여니의 틀린 그림 찾기 이벤트 (~12/17 23:59)",
+    dateRange: "2025년 12월 5일 ~ 2025년 12월 17일 23:59",
+    startDate: "2025. 12. 05 12:00",
+    endDate: "2025. 12. 17 23:59",
+    image: img3,
+    link: "https://cafe.naver.com/com2usbaseball2015/1929170"
+  },
+]
+export default events;
