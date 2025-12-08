2fbe15816c9ddd11bc6ea530486dff4b83468786
Jae-Hong-Kim <newfly101@naver.com>
2025-12-08 09:17:39 +0900

    [data] historymode data 추가

---
+++ web/src/data/historyMode.js
@@ -0,0 +1,552 @@
+const historyModeData = [
+  {
+    day: 1,
+    groups: [
+      {
+        id:1,
+        title: "1982 KBO 원년 라인업",
+        players: [
+          {id:1, name: "이만수", year:1982, position:"1B", isLegend: true, legendName: "김시진"},
+          {id:2, name: "신경식", year:1982, position:"1B", isLegend: false},
+          {id:3, name: "김인식", year:1982, position:"1B", isLegend: true, legendName: "백인천"},
+          {id:4, name: "김용희", year:1982, position:"1B", isLegend: false},
+          {id:5, name: "오대석", year:1982, position:"1B", isLegend: false},
+          {id:6, name: "백인천", year:1982, position:"1B", isLegend: true, legendName: "이종범"},
+          {id:7, name: "김우열", year:1982, position:"1B", isLegend: false},
+          {id:8, name: "이종도", year:1982, position:"1B", isLegend: false},
+          {id:9, name: "김봉연", year:1982, position:"1B", isLegend: false},
+          {id:10, name: "김용달", year:1982, position:"1B", isLegend: false},
+          {id:11, name: "김용운", year:1982, position:"1B", isLegend: false},
+          {id:12, name: "양승관", year:1982, position:"1B", isLegend: false},
+          {id:13, name: "차영화", year:1982, position:"1B", isLegend: false},
+          {id:14, name: "김성관", year:1982, position:"1B", isLegend: false},
+          {id:15, name: "박철순", year:1982, position:"SP", isLegend: true, legendName: "정민태"},
+          {id:16, name: "김용남", year:1982, position:"SP", isLegend: false},
+          {id:17, name: "이선희", year:1982, position:"SP", isLegend: false},
+          {id:18, name: "하기룡", year:1982, position:"SP", isLegend: false},
+          {id:19, name: "유종겸", year:1982, position:"SP", isLegend: false},
+          {id:20, name: "황태환", year:1982, position:"RP", isLegend: false},
+          {id:21, name: "황규봉", year:1982, position:"RP", isLegend: false},
+          {id:22, name: "권영호", year:1982, position:"RP", isLegend: false},
+          {id:23, name: "이상윤", year:1982, position:"RP", isLegend: false},
+          {id:24, name: "노상수", year:1982, position:"RP", isLegend: false},
+          {id:25, name: "김사용", year:1982, position:"CP", isLegend: false},
+        ]
+      },
+      {
+        id:2,
+        title: "1983 삼미 슈퍼스타즈",
+        players: [
+          {id:1, name: "장정기", year:1983, position:"1B", isLegend: false},
+          {id:2, name: "김대진", year:1983, position:"1B", isLegend: false},
+          {id:3, name: "양승관", year:1983, position:"1B", isLegend: false},
+          {id:4, name: "김진우B", year:1983, position:"1B", isLegend: false},
+          {id:5, name: "정구선", year:1983, position:"1B", isLegend: false},
+          {id:6, name: "이선웅", year:1983, position:"1B", isLegend: false},
+          {id:7, name: "조흥운", year:1983, position:"1B", isLegend: false},
+          {id:8, name: "정구왕", year:1983, position:"1B", isLegend: false},
+          {id:9, name: "박준영B", year:1983, position:"1B", isLegend: false},
+          {id:10, name: "금광옥", year:1983, position:"1B", isLegend: false},
+          {id:11, name: "이영구", year:1983, position:"1B", isLegend: false},
+          {id:12, name: "김경남", year:1983, position:"1B", isLegend: false},
+          {id:13, name: "최홍석", year:1983, position:"1B", isLegend: false},
+          {id:14, name: "백인천", year:1983, position:"1B", isLegend: false},
+          {id:15, name: "장명부", year:1983, position:"SP", isLegend: true, legendName: "최동원"},
+          {id:16, name: "임호균", year:1983, position:"SP", isLegend: false},
+          {id:17, name: "김상기", year:1983, position:"SP", isLegend: false},
+          {id:18, name: "유서호", year:1983, position:"SP", isLegend: false},
+          {id:19, name: "정성만", year:1983, position:"SP", isLegend: false},
+          {id:20, name: "오문현", year:1983, position:"RP", isLegend: false},
+          {id:21, name: "인호봉", year:1983, position:"RP", isLegend: false},
+          {id:22, name: "김재현B", year:1983, position:"RP", isLegend: false},
+          {id:23, name: "김사용", year:1983, position:"RP", isLegend: false},
+          {id:24, name: "신태중", year:1984, position:"RP", isLegend: false},
+          {id:25, name: "최계훈", year:1984, position:"CP", isLegend: false},
+        ]
+      },
+      {
+        id:3,
+        title: "1984 해태 타이거즈",
+        players: [
+          {id:1, name: "서정환", year:1984, position:"1B", isLegend: false},
+          {id:2, name: "김일권", year:1984, position:"1B", isLegend: false},
+          {id:3, name: "김성한B", year:1984, position:"1B", isLegend: false},
+          {id:4, name: "김봉연", year:1984, position:"1B", isLegend: false},
+          {id:5, name: "김종모", year:1984, position:"1B", isLegend: false},
+          {id:6, name: "김준환", year:1984, position:"1B", isLegend: false},
+          {id:7, name: "장진범", year:1984, position:"1B", isLegend: false},
+          {id:8, name: "김무종", year:1984, position:"1B", isLegend: false},
+          {id:9, name: "차영화", year:1984, position:"1B", isLegend: false},
+          {id:10, name: "유승안", year:1984, position:"1B", isLegend: false},
+          {id:11, name: "김일환", year:1984, position:"1B", isLegend: false},
+          {id:12, name: "조충열", year:1984, position:"1B", isLegend: false},
+          {id:13, name: "송일섭", year:1984, position:"1B", isLegend: false},
+          {id:14, name: "김종윤", year:1984, position:"1B", isLegend: false},
+          {id:15, name: "방수원", year:1984, position:"SP", isLegend: false},
+          {id:16, name: "김용남", year:1984, position:"SP", isLegend: false},
+          {id:17, name: "강만식", year:1984, position:"SP", isLegend: false},
+          {id:18, name: "주동식", year:1984, position:"SP", isLegend: false},
+          {id:19, name: "이상윤", year:1984, position:"SP", isLegend: false},
+          {id:20, name: "문희수", year:1984, position:"RP", isLegend: false},
+          {id:21, name: "조도연", year:1984, position:"RP", isLegend: false},
+          {id:22, name: "김현재", year:1984, position:"RP", isLegend: false},
+          {id:23, name: "신태순", year:1984, position:"RP", isLegend: false},
+          {id:24, name: "김기철", year:1984, position:"RP", isLegend: false},
+          {id:25, name: "황기선", year:1984, position:"CP", isLegend: false},
+        ]
+      },
+      {
+        id:3,
+        title: "1984 롯데 자이언츠",
+        players: [
+          {id:1, name: "김재상B", year:1984, position:"1B", isLegend: false},
+          {id:2, name: "홍문종", year:1984, position:"1B", isLegend: false},
+          {id:3, name: "김용희B", year:1984, position:"1B", isLegend: false},
+          {id:4, name: "김용철", year:1984, position:"1B", isLegend: false},
+          {id:5, name: "유두열", year:1984, position:"1B", isLegend: true, legendName: "최동원"},
+          {id:6, name: "박용성", year:1984, position:"1B", isLegend: false},
+          {id:7, name: "한문연", year:1984, position:"1B", isLegend: false},
+          {id:8, name: "박영태", year:1984, position:"1B", isLegend: false},
+          {id:9, name: "김민호B", year:1984, position:"1B", isLegend: false},
+          {id:10, name: "심재원", year:1984, position:"1B", isLegend: false},
+          {id:11, name: "정영기", year:1984, position:"1B", isLegend: false},
+          {id:12, name: "이승하", year:1984, position:"1B", isLegend: false},
+          {id:13, name: "조성옥", year:1984, position:"1B", isLegend: false},
+          {id:14, name: "김한조", year:1984, position:"1B", isLegend: false},
+          {id:15, name: "최동원", year:1984, position:"SP", isLegend: true, legendName: "김시진"},
+          {id:16, name: "박두희", year:1984, position:"SP", isLegend: false},
+          {id:17, name: "김문희", year:1984, position:"SP", isLegend: false},
+          {id:18, name: "안창완", year:1984, position:"SP", isLegend: false},
+          {id:19, name: "임호균", year:1984, position:"SP", isLegend: false},
+          {id:20, name: "김재열B", year:1984, position:"RP", isLegend: false},
+          {id:21, name: "천창호", year:1984, position:"RP", isLegend: false},
+          {id:22, name: "이진우", year:1984, position:"RP", isLegend: false},
+          {id:23, name: "이문한", year:1984, position:"RP", isLegend: false},
+          {id:24, name: "이윤섭", year:1984, position:"RP", isLegend: false},
+          {id:25, name: "배경환", year:1984, position:"CP", isLegend: false},
+        ]
+      },
+      {
+        id:5,
+        title: "1985 삼성 라이온즈",
+        players: [
+          {id:1, name: "이해창", year:1985, position:"1B", isLegend: false},
+          {id:2, name: "허규옥", year:1985, position:"1B", isLegend: false},
+          {id:3, name: "장효조", year:1985, position:"1B", isLegend: false},
+          {id:4, name: "이만수", year:1985, position:"1B", isLegend: true, legendName: "장종훈"},
+          {id:5, name: "박승호", year:1985, position:"1B", isLegend: false},
+          {id:6, name: "김성래", year:1985, position:"1B", isLegend: false},
+          {id:7, name: "함학수", year:1985, position:"1B", isLegend: false},
+          {id:8, name: "김용국", year:1985, position:"1B", isLegend: false},
+          {id:9, name: "배대웅", year:1985, position:"1B", isLegend: false},
+          {id:10, name: "송일수", year:1985, position:"1B", isLegend: false},
+          {id:11, name: "이종두", year:1985, position:"1B", isLegend: false},
+          {id:12, name: "오대석", year:1985, position:"1B", isLegend: false},
+          {id:13, name: "정현발", year:1985, position:"1B", isLegend: false},
+          {id:14, name: "장태수B", year:1985, position:"1B", isLegend: false},
+          {id:15, name: "김시진", year:1985, position:"SP", isLegend: true, legendName: "정명원"},
+          {id:16, name: "김일융", year:1985, position:"SP", isLegend: true, legendName: "김시진"},
+          {id:17, name: "황규봉", year:1985, position:"SP", isLegend: false},
+          {id:18, name: "진동한", year:1985, position:"SP", isLegend: false},
+          {id:19, name: "양일환", year:1985, position:"SP", isLegend: false},
+          {id:20, name: "송진우", year:1985, position:"RP", isLegend: false},
+          {id:21, name: "성낙수", year:1984, position:"RP", isLegend: false},
+          {id:22, name: "권기홍", year:1986, position:"RP", isLegend: false},
+          {id:23, name: "김도진", year:1986, position:"RP", isLegend: false},
+          {id:24, name: "전용권", year:1986, position:"RP", isLegend: false},
+          {id:25, name: "권영호", year:1985, position:"CP", isLegend: false},
+        ]
+      }
+    ]
+  },
+  {
+    day: 2,
+    groups: [
+      {
+        id: 1,
+        title: "1986 빙그래 이글스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "1986 MBC 청룡",
+        players: []
+      },
+      {
+        id: 3,
+        title: "1986 해태 타이거즈",
+        players: []
+      },
+      {
+        id: 4,
+        title: "1987 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 5,
+        title: "1987 삼성 라이온즈",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 3,
+    groups: [
+      {
+        id: 1,
+        title: "1988 빙그래 이글스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "1989 태평양 돌핀스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "1989 해태 타이거즈",
+        players: []
+      },
+      {
+        id: 4,
+        title: "1991 쌍방울 레이더스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "1990 LG 트윈스",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 4,
+    groups: [
+      {
+        id: 1,
+        title: "1992 빙그래 이글스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "1992 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 3,
+        title: "1993 OB 베어스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "1993 해태 타이거즈",
+        players: []
+      },
+      {
+        id: 5,
+        title: "1994 LG 트윈스",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 5,
+    groups: [
+      {
+        id: 1,
+        title: "1995 태평양 돌핀스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "1995 OB 베어스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "1996 현대 유니콘스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "1997 OB 베어스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "1997 쌍방울 레이더스",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 6,
+    groups: [
+      {
+        id: 1,
+        title: "1997 해태 타이거즈",
+        players: []
+      },
+      {
+        id: 2,
+        title: "1998 KBO 역대 용병 라인업",
+        players: []
+      },
+      {
+        id: 3,
+        title: "1999 LG 트윈스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "1999 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 5,
+        title: "1999 한화 이글스",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 7,
+    groups: [
+      {
+        id: 1,
+        title: "1999 KBO 역대 FA 라인업",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2000 현대 유니콘스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2001 두산 베어스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2002 LG 트윈스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2002 삼성 라이온즈",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 8,
+    groups: [
+      {
+        id: 1,
+        title: "2003 삼성 라이온즈",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2004 현대 유니콘스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2005 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2006 한화 이글스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2007 SK 와이번스",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 9,
+    groups: [
+      {
+        id: 1,
+        title: "2008 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2008 서울 히어로즈",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2008 두산 베어스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2009 KIA 타이거즈",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2010 삼성 라이온즈",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 10,
+    groups: [
+      {
+        id: 1,
+        title: "2010 SK 와이번스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2010 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2013 NC 다이노스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2011 삼성 라이온즈",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2011 KIA 타이거즈",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 11,
+    groups: [
+      {
+        id: 1,
+        title: "2012 한화 이글스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2015 KT 위즈",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2013 LG 트윈스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2014 SK 와이번스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2014 넥센 히어로즈",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 12,
+    groups: [
+      {
+        id: 1,
+        title: "2014 삼성 라이온즈",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2015 NC 다이노스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2016 두산 베어스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2017 롯데 자이언츠",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2017 이승엽/이호준 은퇴경기 라인업",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 13,
+    groups: [
+      {
+        id: 1,
+        title: "2017 KIA 타이거즈",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2018 한화 이글스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2018 SK 와이번스",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2019 두산 베어스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2020 주요 선수 라인업",
+        players: []
+      }
+    ]
+  },
+  {
+    day: 14,
+    groups: [
+      {
+        id: 1,
+        title: "2020 LG 트윈스",
+        players: []
+      },
+      {
+        id: 2,
+        title: "2020 NC 다이노스",
+        players: []
+      },
+      {
+        id: 3,
+        title: "2021 KT 위즈",
+        players: []
+      },
+      {
+        id: 4,
+        title: "2022 SSG 랜더스",
+        players: []
+      },
+      {
+        id: 5,
+        title: "2023 LG 트윈스",
+        players: []
+      }
+    ]
+  }
+]
