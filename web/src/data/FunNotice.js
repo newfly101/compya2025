ec9c4bb5e29f68469d6df701ad29b4ac5a9ad789
Jae-Hong-Kim <newfly101@naver.com>
2025-12-19 17:45:16 +0900

    [data] 공지사항 data 연결

---
+++ web/src/data/FunNotice.js
@@ -0,0 +1,10 @@
+const funNotice = [
+  {
+    id: 1,
+    title: 0,
+    titleName: "컴프야펀 업데이트 안내 v0.1.0",
+    updateDate: "2025-12-19"
+  }
+]
+
+export default funNotice;
--- web/src/pages/notice/FunNoticeList.jsx
+++ web/src/pages/notice/FunNoticeList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import NoticeCard from "@/components/common/NoticeCard.jsx";
+import funNotice from "@/data/FunNotice.js";
 
 const FunNoticeList = () => {
   return (
-    <div>📢 공지사항 리스트</div>
+    <div>📢 공지사항 리스트
+      {funNotice.map((item) => (
+        <NoticeCard id={item.id} title={item.title} date={item.updateDate} info={item.titleName} />
+      ))}
+    </div>
   );
 };
 
