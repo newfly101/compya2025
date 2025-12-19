5f9525401e8c3e1bb60c0782b105559714b1ef03
Jae-Hong-Kim <newfly101@naver.com>
2025-12-19 19:38:56 +0900

    [data] event data 수정

--- web/src/data/Events.js
+++ web/src/data/Events.js
@@ -1,6 +1,9 @@
 import img1 from "@/assets/events/EventoffseasonHeat.jpg";
 import img2 from "@/assets/events/Memories Draw.jpg";
 import img3 from "@/assets/events/Spot the Difference.jpg";
+import img4 from "@/assets/events/compya2draft.jpg";
+import img5 from "@/assets/events/compyachrithmas.jpg";
+import img6 from "@/assets/events/compyamymixture.png";
 
 const events = [
   {
@@ -29,6 +32,33 @@ const events = [
     endDate: "2025. 12. 17 23:59",
     image: img3,
     link: "https://cafe.naver.com/com2usbaseball2015/1929170"
+  },
+  {
+    id: 4,
+    title: "[이벤트] 컴프야 2차 드래프트 이벤트 (12/1~12/31 23:59)",
+    dateRange: "2025년 12월 5일 ~ 2025년 12월 17일 23:59",
+    startDate: "2025. 12. 01 12:00",
+    endDate: "2025. 12. 31 23:59",
+    image: img4,
+    link: "https://cafe.naver.com/com2usbaseball2015/1922725"
+  },
+  {
+    id: 5,
+    title: "[이벤트] 수여니 산타의 미션! 트리 꾸미기 이벤트 (~12/31 23:59)",
+    dateRange: "2025년 12월 18일 ~ 2025년 12월 31일 23:59",
+    startDate: "2025. 12. 18 12:00",
+    endDate: "2025. 12. 31 23:59",
+    image: img5,
+    link: "https://cafe.naver.com/com2usbaseball2015/1931690"
+  },
+  {
+    id: 6,
+    title: "[이벤트] 나만의 조합 이벤트(~12/31)",
+    dateRange: "2025년 12월 19일 ~ 2025년 12월 31일 23:59",
+    startDate: "2025. 12. 19 12:00",
+    endDate: "2025. 12. 31 23:59",
+    image: img6,
+    link: "https://cafe.naver.com/com2usbaseball2015/1931766"
   },
 ]
 export default events;
