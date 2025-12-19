a02bacbfd8c42bf9a3e3ef8c62b459bf6b97dc3d
Jae-Hong-Kim <newfly101@naver.com>
2025-12-19 19:57:05 +0900

    [data] data 정보 추가

--- web/src/data/Events.js
+++ web/src/data/Events.js
@@ -8,55 +8,55 @@ import img6 from "@/assets/events/compyamymixture.png";
 const events = [
   {
     id: 1,
-    title: "[이벤트] 2026시즌 대비 오프시즌 타격 이벤트(11/13~3/28)",
+    title: "[이벤트] 2026시즌 대비 오프시즌 타격 이벤트",
     dateRange: "2025년 11월 13일 12:00 ~ 2026년 3월 28일 23:59",
     startDate: "2025. 11. 13",
-    endDate: "2026. 03. 28",
+    expireDate: "2026. 03. 28 23:59",
     image: img1,
     link: "https://cafe.naver.com/com2usbaseball2015/1923858"
   },
   {
     id: 2,
-    title: "[이벤트] 추억의 뽑기 이벤트(~12/31)",
-    dateRange: "2025년 12월 01일 12:00 ~ 2025년 12월 31일 23:59",
+    title: "[이벤트] 추억의 뽑기 이벤트",
+    dateRange: "2025년 12월 1일 12:00 ~ 2025년 12월 31일 23:59",
     startDate: "2025. 12. 01 12:00",
-    endDate: "2025. 12. 31 23:59",
+    expireDate: "2025. 12. 31 23:59",
     image: img2,
     link: "https://cafe.naver.com/com2usbaseball2015/1928065"
   },
   {
     id: 3,
-    title: "[이벤트] 수여니의 틀린 그림 찾기 이벤트 (~12/17 23:59)",
+    title: "[이벤트] 수여니의 틀린 그림 찾기 이벤트",
     dateRange: "2025년 12월 5일 ~ 2025년 12월 17일 23:59",
     startDate: "2025. 12. 05 12:00",
-    endDate: "2025. 12. 17 23:59",
+    expireDate: "2025. 12. 17 23:59",
     image: img3,
     link: "https://cafe.naver.com/com2usbaseball2015/1929170"
   },
   {
     id: 4,
-    title: "[이벤트] 컴프야 2차 드래프트 이벤트 (12/1~12/31 23:59)",
-    dateRange: "2025년 12월 5일 ~ 2025년 12월 17일 23:59",
+    title: "[이벤트] 컴프야 2차 드래프트 이벤트",
+    dateRange: "2025년 12월 1일 ~ 2025년 12월 31일 23:59",
     startDate: "2025. 12. 01 12:00",
-    endDate: "2025. 12. 31 23:59",
+    expireDate: "2025. 12. 31 23:59",
     image: img4,
     link: "https://cafe.naver.com/com2usbaseball2015/1922725"
   },
   {
     id: 5,
-    title: "[이벤트] 수여니 산타의 미션! 트리 꾸미기 이벤트 (~12/31 23:59)",
+    title: "[이벤트] 수여니 산타의 미션! 트리 꾸미기 이벤트",
     dateRange: "2025년 12월 18일 ~ 2025년 12월 31일 23:59",
     startDate: "2025. 12. 18 12:00",
-    endDate: "2025. 12. 31 23:59",
+    expireDate: "2025. 12. 31 23:59",
     image: img5,
     link: "https://cafe.naver.com/com2usbaseball2015/1931690"
   },
   {
     id: 6,
-    title: "[이벤트] 나만의 조합 이벤트(~12/31)",
+    title: "[이벤트] 나만의 조합 이벤트",
     dateRange: "2025년 12월 19일 ~ 2025년 12월 31일 23:59",
     startDate: "2025. 12. 19 12:00",
-    endDate: "2025. 12. 31 23:59",
+    expireDate: "2025. 12. 31 23:59",
     image: img6,
     link: "https://cafe.naver.com/com2usbaseball2015/1931766"
   },
