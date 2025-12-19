172bcb08c9c2caa781397125e26bf60becb3458e
Jae-Hong-Kim <newfly101@naver.com>
2025-12-19 19:08:29 +0900

    [page] 공지사항 상세 페이지 연결

--- web/src/components/common/NoticeCard.jsx
+++ web/src/components/common/NoticeCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "@/styles/layout/NoticeCard.module.scss";
+import { useNavigate } from "react-router-dom";
 
-const NoticeCard = ({title, date, info}) => {
+const NoticeCard = ({id, title, date, info}) => {
+  const navigate = useNavigate();
+
   const titleValue = title === 0 ? "메키펀 공지사항" : "공식 공지사항";
+
+  const handleClick = () => {
+    navigate(`/notice/${id}`);
+  }
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleClick}>
       <div className={styles.header}>
         {titleValue}
         <span>게시일: {date}</span>
