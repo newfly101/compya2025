14c7c1b975c56cfaaeb5d6266e7e1059c4df42b3
Jae-Hong-Kim <newfly101@naver.com>
2025-12-25 03:37:39 +0900

    [data] 투수 고유 스킬 data 추가

---
+++ web/src/data/skill/PITCKER_SKILLS.js
@@ -0,0 +1,59 @@
+export const PITCHER_SKILLS = {
+  legend: [
+    { id: "L1", name: "슈퍼스타", desc: "" },
+    { id: "L2", name: "팔색조", desc: "" },
+    { id: "L3", name: "라이징 패스트볼", desc: "" },
+    { id: "L4", name: "배테랑", desc: "" },
+    { id: "L5", name: "캡틴", desc: "" },
+    { id: "L6", name: "투혼", desc: "" },
+    { id: "L7", name: "닥터K", desc: "" }
+  ],
+
+  platinum: [
+    { id: "P1", name: "각성", desc: "" },
+    { id: "P2", name: "뒷심", desc: "" },
+    { id: "P3", name: "끝판왕", desc: "" },
+    { id: "P4", name: "자이언트 킬러", desc: "" },
+    { id: "P5", name: "이닝이터", desc: "" },
+    { id: "P6", name: "언터처블", desc: "" },
+    { id: "P7", name: "결정구", desc: "" },
+    { id: "P8", name: "수호신", desc: "" },
+    { id: "P9", name: "버닝", desc: "" },
+    { id: "P10", name: "에이스", desc: "" },
+    { id: "P11", name: "벌떼야구", desc: "" },
+    { id: "P12", name: "결자해지", desc: "" },
+    { id: "P13", name: "투지", desc: "" }
+  ],
+
+  hero: [
+    { id: "H1", name: "승승장구", desc: "" },
+    { id: "H2", name: "컨트롤아티스트", desc: "" },
+    { id: "H3", name: "피칭머신", desc: "" },
+    { id: "H4", name: "마당쇠", desc: "" },
+    { id: "H5", name: "저격수", desc: "" },
+    { id: "H6", name: "전력투구", desc: "" },
+    { id: "H7", name: "해결사", desc: "" },
+    { id: "H8", name: "승부사", desc: "" },
+    { id: "H9", name: "클린업킬러", desc: "" },
+    { id: "H10", name: "파워피쳐", desc: "" },
+    { id: "H11", name: "국보투수", desc: "" },
+    { id: "H12", name: "셋업맨", desc: "" },
+    { id: "H13", name: "야전지휘", desc: "" }
+  ],
+
+  normal: [
+    { id: "N1", name: "견제왕", desc: "" },
+    { id: "N2", name: "위기극복", desc: "" },
+    { id: "N3", name: "얼음장", desc: "" },
+    { id: "N4", name: "돌부처", desc: "" },
+    { id: "N5", name: "광속투구", desc: "" },
+    { id: "N6", name: "평정심", desc: "" },
+    { id: "N7", name: "완급조절", desc: "" },
+    { id: "N8", name: "우타킬러", desc: "" },
+    { id: "N9", name: "좌타킬러", desc: "" },
+    { id: "N10", name: "마구연마", desc: "" },
+    { id: "N11", name: "강심장", desc: "" },
+    { id: "N12", name: "노련함", desc: "" },
+    { id: "N13", name: "강철체력", desc: "" }
+  ]
+};
