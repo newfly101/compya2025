b882a92cba73bfe095f305753ac74ec8ac8322c3
Jae-Hong-Kim <newfly101@naver.com>
2025-12-29 23:19:59 +0900

    [GTM] 컴프야펀 사용자 데이터 수집 연결 작업 (12/29 패치 완료)

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "@/components/Header.jsx";
 import Footer from "@/components/Footer.jsx";
 import styles from "@/styles/layout/appLayout.module.scss";
+import { useEffect } from "react";
 
 const App = () => {
+  const location = useLocation();
+
+  /* Google Analytics 추가 */
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "page_view",
+      page_path: location.pathname,
+      page_location: window.location.href,
+    });
+  }, [location.pathname]);
+
   return (
     <div className={styles.container}>
       <Header />
