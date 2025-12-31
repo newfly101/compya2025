687d0327926747cadbc81ebff7c4f163d7df0636
Jae-Hong-Kim <newfly101@naver.com>
2026-01-01 01:33:24 +0900

    [data] 컴프야 공식카페 공지사항 추가

---
+++ web/src/data/CafeNotice.js
@@ -0,0 +1,29 @@
+import cafeUpdateImg from "@/assets/events/com2usUpdate.png";
+import cafeNoticeImg from "@/assets/events/com2usNotice.png";
+
+export const cafeNotice = [
+  {
+    id: 1,
+    title: "[안내] 2026 새해맞이 특별 패키지 & 고급 고유능력 각성권 패키지 판매 안내",
+    startDate: "2025. 12. 31",
+    expireDate: "2026. 01. 11 23:59",
+    image: cafeNoticeImg,
+    link: "https://cafe.naver.com/com2usbaseball2015/1934678"
+  },
+  {
+    id: 2,
+    title: "[안내] v11.9.0 업데이트 안내 (iOS/Google/Onestore)",
+    startDate: "2025. 12. 18 10:17",
+    expireDate: "2026. 12. 31 23:59",
+    image: cafeUpdateImg,
+    link: "https://cafe.naver.com/com2usbaseball2015/1931684"
+  },
+  {
+    id: 3,
+    title: "[안내] 제 7회 클럽 토너먼트 대회 결과 및 우승 클럽 안내",
+    startDate: "2025. 12. 22 10:17",
+    expireDate: "2026. 12. 31 23:59",
+    image: cafeNoticeImg,
+    link: "https://cafe.naver.com/com2usbaseball2015/1932664"
+  },
+]
