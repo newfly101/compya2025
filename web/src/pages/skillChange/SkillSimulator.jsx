1a2ed1f00d54281aae5774af0079d9befb8edd74
Jae-Hong-Kim <newfly101@naver.com>
2026-01-09 17:17:45 +0900

    [page] 스변 페이지 중간 과정 추가

---
+++ web/src/pages/skillChange/SkillSimulator.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "@/styles/pages/dictionary/Dictionary.module.scss";
+import pitcherImg from "@/assets/dictionary/pitcherImg.png";
+import DictionaryCard from "@/components/common/DictionaryCard.jsx";
+import hitterImg from "@/assets/dictionary/hitterImg.png";
+
+const SkillSimulator = () => {
+  const navigate = useNavigate();
+
+  const handleMoveUrl = () => {
+    navigate("/");
+  };
+
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <span className={styles.category} onClick={handleMoveUrl}>← 메인으로</span>
+        <h1 className={styles.title}>📌 스킬 시뮬레이터</h1>
+
+        <div className={styles.meta}>
+          <span>2026-01-09</span>
+          <span>v0.1.6</span>
+        </div>
+      </header>
+      <div className={styles.cardGrid}>
+        <DictionaryCard
+          icon="🧤"
+          title="투수 고스변 시뮬레이터"
+          desc={["레전드 투수 스킬 시뮬레이터", "투수 선택 가능", ]}
+          link="/simulate/pitcher"
+          image={pitcherImg}
+          disabled={false}
+        />
+        <DictionaryCard
+          icon="⚾"
+          title="타자 고스변 시뮬레이터"
+          desc={["레전드 타자 스킬 시뮬레이터", "타자 선택 가능", ]}
+          link="/simulate/batter"
+          image={hitterImg}
+          disabled={false}
+        />
+      </div>
+
+
+    </main>
+);
+};
+
+export default SkillSimulator;
--- web/src/router/router.jsx
+++ web/src/router/router.jsx
@@ -10,6 +10,7 @@ import PrivacyPolicy from "@/pages/legal/PrivacyPolicy.jsx";
 import SkillDictionary from "@/components/common/page/SkillDictionary.jsx";
 import Dictionary from "@/pages/dictionary/Dictionary.jsx";
 import HitterSkillDictionary from "@/components/common/page/HitterSkillDictionary.jsx";
+import SkillSimulator from "@/pages/skillChange/SkillSimulator.jsx";
 
 const router = createBrowserRouter([
   {
@@ -27,7 +28,9 @@ const router = createBrowserRouter([
       },
       {path: "notice", element: <Notice />},
       {path: "notice/:id", element: <FunNoticePage />},
-      {path: "simulate", element: <SkillChange />, handle: { title: "컴프야펀 | 고스변 시뮬레이터" }},
+      {path: "simulate", element: <SkillSimulator />, handle: { title: "컴프야펀 | 스킬 변경 시뮬레이터" }},
+      {path: "simulate/pitcher", element: <SkillChange />, handle: { title: "컴프야펀 | 투수 고스변 시뮬레이터" }},
+      {path: "simulate/batter", element: <SkillChange />, handle: { title: "컴프야펀 | 타자 고스변 시뮬레이터" }},
       {path: "tips", element: <TipPage />, handle: { title: "컴프야펀 | 팁 모아보기" }},
       {path: "dictionary/pitcher", element: <SkillDictionary />, handle: { title: "컴프야펀 | 투수 스킬 백과사전" }},
       {path: "dictionary/batter", element: <HitterSkillDictionary />, handle: { title: "컴프야펀 | 타자 스킬 백과사전" }},
