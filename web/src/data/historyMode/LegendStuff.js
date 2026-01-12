a25fa556f0f9a23be7232a9f30fff984f2ba342f
Jae-Hong-Kim <newfly101@naver.com>
2026-01-12 12:36:56 +0900

    [data] 히스토리 모드 legend data 입력

---
+++ web/src/data/historyMode/LegendStuff.js
@@ -0,0 +1,653 @@
+export const legendStuff = [
+  {
+    day: 1,
+    name: "한국 프로야구 시작",
+    roaster: 1,
+    item: [
+      { legend: "김시진", player: "이만수", years: 1982 },
+      { legend: "백인천", player: "김인식", years: 1982 },
+      { legend: "이종범", player: "백인천", years: 1982 },
+      { legend: "정민태", player: "박철순", years: 1982 },
+    ],
+  },
+  {
+    day: 1,
+    name: "1983년 삼미 장명부 30승 달성",
+    roaster: 2,
+    item: [
+      { legend: "최동원", player: "장명부", years: 1982 },
+    ],
+  },
+  {
+    day: 1,
+    name: "1984년 해태 방수원 KBO 1호 노히트노런",
+    roaster: 3,
+    item: [
+      {},
+    ],
+  },
+  {
+    day: 1,
+    name: "1984년 최동원 한국시리즈 4승",
+    roaster: 4,
+    item: [
+      { legend: "최동원", player: "유두열", years: 1984 },
+      { legend: "김시진", player: "최동원", years: 1984 },
+    ],
+  },
+  {
+    day: 1,
+    name: "1985년 삼성라이온즈 첫 통합 우승",
+    roaster: 5,
+    item: [
+      { legend: "장종훈", player: "이만수", years: 1985 },
+      { legend: "김시진", player: "김일융", years: 1985 },
+      { legend: "정명원", player: "김시진", years: 1985 },
+    ],
+  },
+  {
+    day: 2,
+    name: "1986년 7번째 구단 빙그레 이글스 창단",
+    roaster: 1,
+    item: [
+      {},
+    ],
+  },
+  {
+    day: 2,
+    name: "1986년 MBC청룡 아쉬운 가을야구 실패",
+    roaster: 2,
+    item: [
+      {},
+    ],
+  },
+  {
+    day: 2,
+    name: "1986년 해태 타이거즈 최초 2회 한국시리즈 우승",
+    roaster: 3,
+    item: [
+      { legend: "이순철", player: "한대화", years: 1986 },
+      { legend: "한대화", player: "이순철", years: 1986 },
+      { legend: "최동원", player: "선동열", years: 1986 },
+    ],
+  },
+  {
+    day: 2,
+    name: "1986년 해태 타이거즈 최초 2회 한국시리즈 우승",
+    roaster: 4,
+    item: [
+      { legend: "선동열", player: "최동원", years: 1987 },
+    ],
+  },
+  {
+    day: 2,
+    name: "1987년 삼성 라이온즈 정규리그 1위 및 준우승",
+    roaster: 5,
+    item: [
+      { legend: "김태균", player: "장효조", years: 1987 },
+      { legend: "이만수", player: "김시진", years: 1987 },
+      { legend: "양준혁", player: "양일환", years: 1987 },
+      { legend: "류중일", player: "오대석", years: 1987 },
+    ],
+  },
+
+  {
+    day: 3,
+    name: "1988년 빙그레 이글스 첫 가을야구 진출",
+    roaster: 1,
+    item: [
+      { legend: "심정수", player: "장종훈", years: 1988 },
+    ],
+  },
+  {
+    day: 3,
+    name: "1989년 태평양 돌핀스 첫 가을야구 진출",
+    roaster: 2,
+    item: [
+      { legend: "이순철", player: "김일권", years: 1989 },
+      { legend: "조계현", player: "박정현", years: 1989 },
+      { legend: "정명원", player: "최창호", years: 1989 },
+    ],
+  },
+  {
+    day: 3,
+    name: "1989년 해태 타이거즈 첫 한국시리즈 4연패",
+    roaster: 3,
+    item: [
+      { legend: "이강철", player: "조계현", years: 1989 },
+      { legend: "박철순", player: "선동열", years: 1989 },
+    ],
+  },
+  {
+    day: 3,
+    name: "1990년 8번째 구단 쌍방울 레이더스 창단",
+    roaster: 4,
+    item: [
+      { legend: "이강철", player: "김기태S", years: 1991 },
+      { legend: "김기태", player: "조규제", years: 1991 },
+    ],
+  },
+  {
+    day: 3,
+    name: "1990년 LG 트윈스 한국시리즈 우승",
+    roaster: 5,
+    item: [
+      { legend: "홍성흔", player: "김동수", years: 1990 },
+      { legend: "백인천", player: "김용수", years: 1990 },
+      { legend: "김용수", player: "정상흠", years: 1990 },
+    ],
+  },
+
+
+  {
+    day: 4,
+    name: "1992년 빙그레 이글스 정규리그 우승",
+    roaster: 1,
+    item: [
+      { legend: "장종훈", player: "이정훈B", years: 1992 },
+      { legend: "이만수", player: "장종훈", years: 1992 },
+      { legend: "정민태", player: "정민철", years: 1992 },
+      { legend: "이강철", player: "송진우", years: 1992 },
+    ],
+  },
+  {
+    day: 4,
+    name: "1992년 롯데 자이언츠 8년 만의 우승",
+    roaster: 2,
+    item: [
+      { legend: "전준호", player: "이종운", years: 1992 },
+      { legend: "윤학길", player: "박동희", years: 1992 },
+      { legend: "정민철", player: "염종석", years: 1992 },
+    ],
+  },
+  {
+    day: 4,
+    name: "1993년 첫 잠실 시리즈 포스트시즌",
+    roaster: 3,
+    item: [
+      { legend: "김원형", player: "김민호S", years: 1993 },
+    ],
+  },
+  {
+    day: 4,
+    name: "1993년 해태 타이거즈 7번째 우승",
+    roaster: 4,
+    item: [
+      { legend: "양준혁", player: "이종범", years: 1993 },
+      { legend: "조계현", player: "선동열", years: 1993 },
+    ],
+  },
+  {
+    day: 4,
+    name: "1994년 LG 트윈스 신바람 야구 전성기",
+    roaster: 5,
+    item: [
+      { legend: "김재현", player: "류지현", years: 1994 },
+      { legend: "이호준", player: "김재현S", years: 1994 },
+      { legend: "김용수", player: "김선진", years: 1994 },
+      { legend: "이상훈", player: "김태원", years: 1994 },
+    ],
+  },
+
+
+  {
+    day: 5,
+    name: "1995년 태평양 돌핀스 매각",
+    roaster: 1,
+    item: [
+      {},
+    ],
+  },
+  {
+    day: 5,
+    name: "1995년 OB베어스 극적인 정규리그 1위와 한국시리즈 우승",
+    roaster: 2,
+    item: [
+      { legend: "이상훈", player: "김상호", years: 1995 },
+      { legend: "이상훈", player: "김상진", years: 1995 },
+      { legend: "니퍼트", player: "장호연", years: 1995 },
+    ],
+  },
+  {
+    day: 5,
+    name: "1996년 현대 박재홍 KBO 30-30  최초 가입",
+    roaster: 3,
+    item: [
+      { legend: "정명원", player: "장광호", years: 1996 },
+    ],
+  },
+  {
+    day: 5,
+    name: "1997년 OB 박철순 은퇴식",
+    roaster: 4,
+    item: [
+      { legend: "정민철", player: "심정수", years: 1997 },
+    ],
+  },
+  {
+    day: 5,
+    name: "1997년 쌍방울 레이더스 마지막 포스트시즌",
+    roaster: 5,
+    item: [
+      { legend: "김기태", player: "박경완", years: 1997 },
+    ],
+  },
+
+
+  {
+    day: 6,
+    name: "1997년 해태 타이거즈 마지막 우승",
+    roaster: 1,
+    item: [
+      { legend: "김재박", player: "이종범", years: 1997 },
+      { legend: "이순철", player: "김창희", years: 1997 },
+      { legend: "이병규", player: "조계현", years: 1997 },
+    ],
+  },
+  {
+    day: 6,
+    name: "1998년 KBO 외국인 선수제도 도입",
+    roaster: 2,
+    item: [
+      { legend: "박경완", player: "나바로", years: 2014 },
+      { legend: "김동주", player: "우즈", years: 1998 },
+      { legend: "우즈", player: "페르난데스", years: 2020 },
+      { legend: "니퍼트", player: "린드블럼", years: 2019 },
+      { legend: "박재홍", player: "주키치", years: 2012 },
+    ],
+  },
+  {
+    day: 6,
+    name: "1999년 김용수 KBO 첫 영구결번식",
+    roaster: 3,
+    item: [
+      { legend: "김재현", player: "이병규", years: 1999 },
+      { legend: "박정태", player: "김상태", years: 1999 },
+      { legend: "이병규", player: "김용수", years: 1999 },
+    ],
+  },
+  {
+    day: 6,
+    name: "1999년 롯데 자이언츠 극적인 한국시리즈 진출",
+    roaster: 4,
+    item: [
+      { legend: "마해영", player: "호세", years: 1999 },
+      { legend: "박정태", player: "김민재", years: 1999 },
+    ],
+  },
+  {
+    day: 6,
+    name: "1999년 한화 이글스 첫번째 우승",
+    roaster: 5,
+    item: [
+      { legend: "장종훈", player: "로마이어", years: 1999 },
+      { legend: "장종훈", player: "구대성", years: 1999 },
+      { legend: "김원형", player: "장종훈", years: 1999 },
+    ],
+  },
+
+
+  {
+    day: 7,
+    name: "KBO리그 FA제도 도입",
+    roaster: 1,
+    item: [
+      { legend: "박용택", player: "손아섭", years: 2018 },
+      { legend: "장성호", player: "양준혁", years: 2001 },
+      { legend: "박진만", player: "권혁", years: 2015 },
+    ],
+  },
+  {
+    day: 7,
+    name: "2000년 현대 유니콘스 압도적 통합 우승",
+    roaster: 2,
+    item: [
+      { legend: "정민태", player: "임선동", years: 2000 },
+      { legend: "정민태", player: "김수경", years: 2000 },
+    ],
+  },
+  {
+    day: 7,
+    name: "2001년 두산 베어스 역대 최저 승률 우승",
+    roaster: 3,
+    item: [
+      { legend: "심정수", player: "심재학", years: 2001 },
+      { legend: "우즈", player: "김동주B", years: 2001 },
+      { legend: "박경완", player: "홍성흔", years: 2001 },
+      { legend: "홍성흔", player: "진필중", years: 2001 },
+    ],
+  },
+  {
+    day: 7,
+    name: "2002년 LG트윈스 4년만에 한국시리즈 진출",
+    roaster: 4,
+    item: [
+      { legend: "마해영", player: "최원호", years: 2002 },
+      { legend: "이승엽", player: "이상훈C", years: 2002 },
+    ],
+  },
+  {
+    day: 7,
+    name: "2002년 삼성라이온즈 첫 한국시리즈 우승",
+    roaster: 5,
+    item: [
+      { legend: "이상훈", player: "이승엽", years: 2002 },
+      { legend: "이승엽", player: "양준혁", years: 2002 },
+    ],
+  },
+
+
+  {
+    day: 8,
+    name: "2003년 이승엽 아시아 최다 56홈런 신기록",
+    roaster: 1,
+    item: [
+      { legend: "심정수", player: "이승엽", years: 2003 },
+      { legend: "한대화", player: "김한수", years: 2003 },
+      { legend: "임찬용", player: "배영수", years: 2003 },
+    ],
+  },
+  {
+    day: 8,
+    name: "2004년 현대 유니콘스 마지막 우승",
+    roaster: 2,
+    item: [
+      { legend: "심정수", player: "박진만", years: 2004 },
+      { legend: "박진만", player: "심정수", years: 2004 },
+    ],
+  },
+  {
+    day: 8,
+    name: "2005년 롯데 손민한 첫 포스트시즌 탈락팀 MVP",
+    roaster: 3,
+    item: [
+      { legend: "배영수", player: "손민환", years: 2005 },
+    ],
+  },
+  {
+    day: 8,
+    name: "2006년 한화 류현진 MVP 신인상 골든글러브 동시 수상",
+    roaster: 4,
+    item: [
+      { legend: "김태균", player: "류현진", years: 2006 },
+      { legend: "배영수", player: "구대성", years: 2006 },
+    ],
+  },
+  {
+    day: 8,
+    name: "2007년 SK 와이번스 첫 우승",
+    roaster: 5,
+    item: [
+      {}
+    ],
+  },
+
+
+  {
+    day: 9,
+    name: "2007년 KBO 첫 외국인 감독 롯데 로이스터 부임",
+    roaster: 1,
+    item: [
+      { legend: "마해영", player: "이대호", years: 2008 },
+    ],
+  },
+  {
+    day: 9,
+    name: "2008년 히어로즈 창단 ",
+    roaster: 2,
+    item: [
+      { legend: "송진우", player: "송지만", years: 2008 },
+    ],
+  },
+  {
+    day: 9,
+    name: "2008년 두산베어스 2년 연속 한국시리즈 진출",
+    roaster: 3,
+    item: [
+      { legend: "박진만", player: "고영민", years: 2008 },
+      { legend: "장종훈", player: "김현수B", years: 2008 },
+    ],
+  },
+  {
+    day: 9,
+    name: "2009년 KIA 타이거즈 12년 만의 우승",
+    roaster: 4,
+    item: [
+      { legend: "김성한", player: "나지완", years: 2009 },
+      { legend: "김성한", player: "김상현S", years: 2009 },
+      { legend: "정근우", player: "서재응", years: 2009 },
+    ],
+  },
+  {
+    day: 9,
+    name: "2010년 삼성 레전드 양준혁 은퇴",
+    roaster: 5,
+    item: [
+      { legend: "장효조", player: "양준혁", years: 2010 },
+      { legend: "구대성", player: "조동찬", years: 2010 },
+    ],
+  },
+
+
+  {
+    day: 10,
+    name: "2010년 SK 와이번스 V3",
+    roaster: 1,
+    item: [
+      { legend: "이만수", player: "박경완", years: 2010 },
+      { legend: "양준혁", player: "송은범", years: 2010 },
+      { legend: "박경완", player: "김광현", years: 2010 },
+      { legend: "김원형", player: "박정권", years: 2010 },
+    ],
+  },
+  {
+    day: 10,
+    name: "2010년 롯데 자이언츠 이대호 타격 7관왕과 MVP 수상",
+    roaster: 2,
+    item: [
+      { legend: "홍성흔", player: "조성환", years: 2010 },
+      { legend: "정민태", player: "이대호", years: 2010 },
+    ],
+  },
+  {
+    day: 10,
+    name: "2011년 NC 다이노스 창단",
+    roaster: 3,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 10,
+    name: "2011년 삼성 라이온즈 5번째 우승",
+    roaster: 4,
+    item: [
+      { legend: "류중일", player: "장원삼", years: 2011 },
+    ],
+  },
+  {
+    day: 10,
+    name: "2012년 KIA 레전드 이종범 은퇴",
+    roaster: 5,
+    item: [
+      { legend: "전준호", player: "이종범", years: 2011 },
+      { legend: "구대성", player: "윤석민", years: 2011 },
+    ],
+  },
+
+
+  {
+    day: 11,
+    name: "2012년 한화 박찬호 KBO 복귀",
+    roaster: 1,
+    item: [
+      { legend: "이대호", player: "김태균S", years: 2012 },
+      { legend: "한대화", player: "최진행", years: 2012 },
+    ],
+  },
+  {
+    day: 11,
+    name: "2012년 KT 위즈 창단",
+    roaster: 2,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 11,
+    name: "2013년 LG 이병규 최고령 사이클링 히트 달성",
+    roaster: 3,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 11,
+    name: "2014년 SK 레전드 박경완 은퇴식",
+    roaster: 4,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 11,
+    name: "2014년 넥센 히어로즈 돌풍",
+    roaster: 5,
+    item: [
+      { legend: "정근우", player: "서건창", years: 2014 },
+      { legend: "박한이", player: "한현희", years: 2014 },
+    ],
+  },
+
+
+  {
+    day: 12,
+    name: "2014 삼성라이온즈 통합우승 4연패",
+    roaster: 1,
+    item: [
+      { legend: "박경완", player: "나바로", years: 2014 },
+    ],
+  },
+  {
+    day: 12,
+    name: "2015년 NC 다이노스 플레이오프 진출",
+    roaster: 2,
+    item: [
+      { legend: "박재홍", player: "템스", years: 2015 },
+      { legend: "이호준", player: "이종욱", years: 2015 },
+    ],
+  },
+  {
+    day: 12,
+    name: "2016년 판타스틱 4 두산 베에스 우승",
+    roaster: 3,
+    item: [
+      { legend: "임창용", player: "이지욱", years: 2016 },
+      { legend: "이병규", player: "니퍼트", years: 2016 },
+    ],
+  },
+  {
+    day: 12,
+    name: "2017년 롯데 자이언츠 5년 만에 가을 야구 진출",
+    roaster: 4,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 12,
+    name: "2017년 삼성 이승엽 NC 이호준 은퇴",
+    roaster: 5,
+    item: [
+      { legend: "이대호", player: "이승엽", years: 2017 },
+    ],
+  },
+
+
+  {
+    day: 13,
+    name: "2017년 기아 타이거즈 V11",
+    roaster: 1,
+    item: [
+      { legend: "이상훈", player: "양현종", years: 2017 },
+      { legend: "이범호", player: "헥터", years: 2017 },
+    ],
+  },
+  {
+    day: 13,
+    name: "2018년 한화 이글스 11년 만의 가을야구",
+    roaster: 2,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 13,
+    name: "2018년 SK 와이번스 우승",
+    roaster: 3,
+    item: [
+      { legend: "김동주", player: "김동엽", years: 2018 },
+    ],
+  },
+  {
+    day: 13,
+    name: "2019년 두산베어스 V6",
+    roaster: 4,
+    item: [
+      { legend: "전준호", player: "정수빈", years: 2019 },
+      { legend: "니퍼트", player: "린드블럼", years: 2019 },
+      { legend: "배영수", player: "권혁", years: 2019 },
+    ],
+  },
+  {
+    day: 13,
+    name: "2020년 사상 최초 코로나로 인한 무관중 시즌",
+    roaster: 5,
+    item: [
+      { legend: "우즈", player: "페르난데스", years: 2020 },
+      { legend: "박한이", player: "박용택", years: 2020 },
+    ],
+  },
+
+
+  {
+    day: 14,
+    name: "2020년 LG트윈스 박용택 통산 2504안타 달성",
+    roaster: 1,
+    item: [
+      { legend: "박한이", player: "박용택", years: 2020 },
+    ],
+  },
+  {
+    day: 14,
+    name: "2020년 NC 다이노스 첫번째 우승",
+    roaster: 2,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 14,
+    name: "2021년 KT 위즈 첫번째 우승",
+    roaster: 3,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 14,
+    name: "2022년 SSG 랜더스 와이어 투 와이어 우승",
+    roaster: 4,
+    item: [
+      {}
+    ],
+  },
+  {
+    day: 14,
+    name: "2023년 LG 트윈스 29년 만의 우승",
+    roaster: 5,
+    item: [
+      {}
+    ],
+  },
+];
