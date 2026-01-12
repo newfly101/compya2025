8d12d7b91ddd1ca8886dc873a586e62bcee34409
Jae-Hong-Kim <newfly101@naver.com>
2026-01-12 21:25:38 +0900

    [refactoring][semantics] FunNoticeList css 파일 분리

--- web/src/feature/notice/components/FunNoticeList.jsx
+++ web/src/feature/notice/components/FunNoticeList.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import NoticeCard from "@/feature/notice/components/cards/NoticeCard.jsx";
 import funNotice from "@/data/FunNotice.js";
-import styles from "@/styles/layout/CouponCard.module.scss";
+import styles from "./FunNoticeList,module.scss";
 
 const FunNoticeList = () => {
   return (
-    <>
-      📢 공지사항 리스트
+    <section aria-labelledby="compyafun-notice-list-title">
+      <h3 id="compyafun-notice-list-title">📢 컴프야펀 공지사항</h3>
       <div className={styles.listGrid}>
         {funNotice.map((item) => (
           <NoticeCard key={item.id} id={item.id} title={item.title} date={item.updateDate} info={item.titleName} />
         ))}
       </div>
-    </>
+    </section>
   );
 };
 
