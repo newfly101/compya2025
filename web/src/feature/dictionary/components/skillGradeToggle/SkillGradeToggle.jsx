66edd564534d83ada7b7bc0a28e05ef950c0d0bd
Jae-Hong-Kim <newfly101@naver.com>
2026-01-14 00:07:13 +0900

    [refactoring][fix] SkillGradeToggle css 오탈자 수정

--- web/src/feature/dictionary/components/skillGradeToggle/SkillGradeToggle.jsx
+++ web/src/feature/dictionary/components/skillGradeToggle/SkillGradeToggle.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./SkillGradeToggle,module.scss";
+import styles from "./SkillGradeToggle.module.scss";
 
 const SkillGradeToggle = ({tier, selected, initSelected, onRecommend}) => {
   return (
