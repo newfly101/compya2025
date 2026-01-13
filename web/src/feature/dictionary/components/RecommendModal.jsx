544e539c0d5f51885dee5cdb10f4879f3bdafa58
Jae-Hong-Kim <newfly101@naver.com>
2026-01-13 23:37:33 +0900

    [refactoring][fix] 변경된 파일 경로 수정

--- web/src/feature/dictionary/components/RecommendModal.jsx
+++ web/src/feature/dictionary/components/RecommendModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import RecommendCard from "@/feature/dictionary/components/cards/recommendCard/RecommendCard.jsx";
-import NoRecommendSkillCard from "@/feature/dictionary/components/cards/NoRecommendSkillCard.jsx";
+import NoRecommendCard from "@/feature/dictionary/components/cards/noRecommendCard/NoRecommendCard.jsx";
 
 const RecommendModal = ({
                           isOpen,
@@ -19,8 +19,8 @@ const RecommendModal = ({
       onClose={onClose}
     />
   ) : (
-    <NoRecommendSkillCard
-      skill={selected.join(" + ")}
+    <NoRecommendCard
+      selected={selected.join(" + ")}
       onClose={onClose}
       mainText="해당 스킬 조합은 잘 사용되지 않습니다."
       subText="다른 스킬 조합을 추천드립니다."
