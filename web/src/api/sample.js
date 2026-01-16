51bbee144f32519b8ef570573adc910b9650a06c
Jae-Hong-Kim <newfly101@naver.com>
2026-01-16 18:25:53 +0900

    [api] 사용자 page 전환 시 uri 보낼 수 있도록 header 에 추가

--- web/src/api/sample.js
+++ web/src/api/sample.js
@@ -4,13 +4,22 @@ const api = axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     'Content-Type': 'application/json',
-    "X-Page-Path": window.location.pathname
+    // "X-Page-Path": window.location.pathname
   },
   timeout: 5000,
   withCredentials: true,
 });
 
+api.interceptors.request.use(
+  (config) => {
+    config.headers["X-Page-Path"] = window.location.pathname;
+    config.headers["X-Referrer"] = document.referrer || "-";
+    config.headers["X-Page-Url"] = window.location.href;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export const fetchHealth = async () => {
-  console.log("Fetching health");
   return await api.get("/health");
 }
