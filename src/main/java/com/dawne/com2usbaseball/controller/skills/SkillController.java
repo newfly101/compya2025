c822300d4598c9dbb10908d07743584794ae8734
Jae-Hong-Kim <newfly101@naver.com>
2026-01-19 00:01:01 +0900

    [DELETE] 불필요한 로그 주석 제거

--- src/main/java/com/dawne/com2usbaseball/controller/skills/SkillController.java
+++ src/main/java/com/dawne/com2usbaseball/controller/skills/SkillController.java
@@ -4,21 +4,16 @@ import com.dawne.com2usbaseball.dto.response.skills.SkillSetResponse;
 import com.dawne.com2usbaseball.enums.Target;
 import com.dawne.com2usbaseball.service.skills.PlayerSkillsService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/skills")
 public class SkillController {
-    private static final Logger log = LoggerFactory.getLogger(SkillController.class);
     private final PlayerSkillsService service;
 
     @GetMapping("/{target}")
     public SkillSetResponse playerTypeSkills(@PathVariable Target target) {
-        SkillSetResponse SAMPLE = service.getPlayerSkillSet(target);
-        log.info("result={}",SAMPLE);
         return service.getPlayerSkillSet(target);
     }
 }
