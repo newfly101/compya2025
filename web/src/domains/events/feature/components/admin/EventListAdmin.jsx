e172526067610d7b59d474f771b3f4294e38e6a2
Jae-Hong-Kim <newfly101@naver.com>
2026-01-31 10:04:37 +0900

    [component][layout] 중복되는 코드 수정을 위해 style 및 component 분리 및 1차 조정

--- web/src/domains/events/feature/components/admin/EventListAdmin.jsx
+++ web/src/domains/events/feature/components/admin/EventListAdmin.jsx
@@ -4,6 +4,7 @@ import { useEventListAdmin } from "@/domains/events/feature/index.js";
 import EventCreateModal from "@/domains/events/feature/components/user/modal/EventCreateModal.jsx";
 import EventEditModal from "@/domains/events/feature/components/user/modal/EventEditModal.jsx";
 import EventVisibilityToggle from "@/domains/events/feature/components/user/support/EventVisibilityToggle.jsx";
+import AdminTableLayout from "@/global/layout/adminLayout/AdminTableLayout.jsx";
 
 const EVENT_COLUMNS = [
   { key: "id", label: "ID" },
@@ -24,57 +25,50 @@ const EventListAdmin = () => {
   const hasEvents = events.length > 0;
 
   return (
-    <div className={styles.wrapper}>
-      <button className={styles.create} onClick={() => setOpen(true)}>이벤트 등록</button>
-
-      <table className={styles.table}>
-        <thead>
+    <AdminTableLayout
+      actions={<button className={styles.create} onClick={() => setOpen(true)}>이벤트 등록</button>}
+      head={
         <tr>
           {EVENT_COLUMNS.map(col => (
             <th key={col.key}>{col.label}</th>
           ))}
         </tr>
-        </thead>
-        <tbody>
-        {!hasEvents && (
+      }
+      body={
+        hasEvents ? events.map(e => {
+            const isExpired = new Date(e.expireAt) <= nowDate;
+            return (
+              <tr key={e.id}>
+                <td>{e.id}</td>
+                <td>{e.title}</td>
+                <td><img src={e.imageUrl} alt={`event-${e.id}`} className={styles.thumbnail} /></td>
+                <td><a href={e.externalLink} target="_blank" rel="noreferrer"> 카페링크 </a></td>
+                <td>{e.startAt} ~ {e.expireAt}</td>
+                <td>{!isExpired ? "진행중" : "종료"}</td>
+                <td><EventVisibilityToggle visible={e.visible} onChange={handleVisibleChange(e.id)} /></td>
+                <td className={styles.actions}>
+                  <button onClick={() => setEditEvent(e)}>수정</button>
+
+                  {editEvent && (
+                    <EventEditModal
+                      event={editEvent}
+                      onClose={() => setEditEvent(false)}
+                    />
+                  )}
+                  <button className={styles.danger}>비공개</button>
+                </td>
+              </tr>
+            );
+          })
+          :
           <tr>
             <td colSpan={8} className={styles.empty}>
               등록된 이벤트가 없습니다.
             </td>
           </tr>
-        )}
-
-        {hasEvents && events.map(e => {
-          const isExpired = new Date(e.expireAt) <= nowDate;
-          return (
-            <tr key={e.id}>
-              <td>{e.id}</td>
-              <td>{e.title}</td>
-              <td><img src={e.imageUrl} alt={`event-${e.id}`} className={styles.thumbnail} /></td>
-              <td><a href={e.externalLink} target="_blank" rel="noreferrer"> 카페링크 </a></td>
-              <td>{e.startAt} ~ {e.expireAt}</td>
-              <td>{!isExpired ? "진행중" : "종료"}</td>
-              <td><EventVisibilityToggle visible={e.visible} onChange={handleVisibleChange(e.id)}/></td>
-              <td className={styles.actions}>
-                <button onClick={() => setEditEvent(e)}>수정</button>
-
-                {editEvent && (
-                  <EventEditModal
-                    event={editEvent}
-                    onClose={() => setEditEvent(false)}
-                  />
-                )}
-                <button className={styles.danger}>비공개</button>
-              </td>
-            </tr>
-          );
-        })
-        }
-        </tbody>
-      </table>
-
-      {open && <EventCreateModal onClose={() => setOpen(false)} />}
-    </div>
+      }
+      children={open && <EventCreateModal onClose={() => setOpen(false)} />}
+    />
   );
 };
 
