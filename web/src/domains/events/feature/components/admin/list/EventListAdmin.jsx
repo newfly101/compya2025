056616bd6e1cb9b67708044a51f974355f33252b
Jae-Hong-Kim <newfly101@naver.com>
2026-01-31 13:48:01 +0900

    [core][fix] filter안에 action (등록) 버튼 추가

--- web/src/domains/events/feature/components/admin/list/EventListAdmin.jsx
+++ web/src/domains/events/feature/components/admin/list/EventListAdmin.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
 import { useEventListAdmin } from "@/domains/events/feature/index.js";
-import EventCreateModal from "@/domains/events/feature/components/admin/modal/EventCreateModal.jsx";
 import AdminTableLayout from "@/global/layout/adminLayout/AdminTableLayout.jsx";
 import EventTableHead from "@/domains/events/feature/components/admin/table/EventTableHead.jsx";
 import EventTableBody from "@/domains/events/feature/components/admin/table/EventTableBody.jsx";
 import EventEditModal from "@/domains/events/feature/components/admin/modal/EventEditModal.jsx";
-import AdminActionButton from "@/global/layout/adminLayout/AdminActionButton.jsx";
+import EventCreateModal from "@/domains/events/feature/components/admin/modal/EventCreateModal.jsx";
+import { searchFilterUnit, statusFilterUnit, visibleFilterUnit } from "@/core/filters";
+import AdminFilterBar from "@/global/layout/adminLayout/AdminFilterBar.jsx";
+import { useFilterPipline } from "@/core/filters/hooks/useFilterPipline.js";
 
 const EventListAdmin = () => {
   const { events, changeVisible } = useEventListAdmin();
   const [open, setOpen] = useState(false);
   const [editEvent, setEditEvent] = useState(false);
 
+  const filterUnits = [
+    searchFilterUnit,
+    statusFilterUnit,
+    visibleFilterUnit,
+  ];
+
+  const { filters, setFilters, filteredData: filteredEvents }
+    = useFilterPipline(events, filterUnits);
+
   return (
     <>
       <AdminTableLayout
-        actions={<AdminActionButton onClick={() => setOpen(true)} actionTitle={"이벤트 등록"} /> }
+        filters={<AdminFilterBar title={"이벤트"} filters={filters} setFilters={setFilters} filterUnits={filterUnits} action={() => setOpen(true)}/>}
         head={<EventTableHead />}
         tbody={
           <EventTableBody
-            events={events}
+            events={filteredEvents}
             changeVisible={changeVisible}
             setEditEvent={setEditEvent}
           />
