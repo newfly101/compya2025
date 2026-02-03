ef550dd8b76b6a5ff529d3345b597b8ca5278e56
Jae-Hong-Kim <newfly101@naver.com>
2026-02-03 13:19:35 +0900

    [page] header 에서 정상적으로 event 불러오도록 변경

--- web/src/global/layout/appLayout/Header.jsx
+++ web/src/global/layout/appLayout/Header.jsx
@@ -39,11 +39,12 @@ export default function Header() {
           <Link to="/">홈</Link>
           {/*<Link to="/damage">데미지 계산기</Link>*/}
           {/*<Link to="/skill">스킬 계산기</Link>*/}
-          <Link to="/notice?tab=event">이벤트</Link>
+          <Link to="/notice?tab=events">이벤트</Link>
           <Link to="/notice?tab=coupons">쿠폰 코드</Link>
           <Link to="/notice">공지사항</Link>
           <Link to="/tips">팁 모아보기</Link>
           <Link to="/dictionary">📌추천 백과사전</Link>
+          <Link to="/community">커뮤니티</Link>
           {isAuthenticated &&
             <Link to="/mypage">마이페이지</Link>
           }
