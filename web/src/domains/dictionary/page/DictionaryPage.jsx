5198969e16dd795cf0de9253a81df88c6f836e3c
Jae-Hong-Kim <newfly101@naver.com>
2026-02-04 17:46:15 +0900

    [config] 상위 page에서 config 랑 data 넣어야 할 요소 분기 처리

--- web/src/domains/dictionary/page/DictionaryPage.jsx
+++ web/src/domains/dictionary/page/DictionaryPage.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { Navigate, useLocation, useParams } from "react-router-dom";
 import { DICTIONARY_HOME_META } from "@/meta/pages/dictionary.meta.js";
 import DictionaryViews from "@/domains/dictionary/feature/components/views/DictionaryViews.jsx";
+import { DICTIONARY_PLAYER_CONFIG } from "@/domains/dictionary/feature/config/player/dictionaryPlayerConfig.js";
 
 const DictionaryPage = () => {
   const { pathname } = useLocation();
 
   const type = pathname.split("/").pop();
   const meta = DICTIONARY_HOME_META.contexts[type];
+  const config = DICTIONARY_PLAYER_CONFIG[type];
 
   // 미구현 NavigationCard 실수로 open 시 변경
   if (!meta) {
     return <Navigate to="/dictionary" replace />;
   }
 
-  return <DictionaryViews />;
+  return <DictionaryViews config={config} />;
 };
 
 export default DictionaryPage;
