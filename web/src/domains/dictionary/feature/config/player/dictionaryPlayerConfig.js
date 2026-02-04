f0e8fd49744f5f1eeb1408e018d9e22b923cad0b
Jae-Hong-Kim <newfly101@naver.com>
2026-02-04 17:39:14 +0900

    [config] 현재 mock_data 로 구성된 부분 그대로 삽입해서 DictionaryView 에서 구분해서 값 삽입할 수 있도록 변경

---
+++ web/src/domains/dictionary/feature/config/player/dictionaryPlayerConfig.js
@@ -0,0 +1,16 @@
+import { HITTER_RECOMMEND } from "@/data/skill/HITTER_RECOMMEND.js";
+import { HITTER_SKILL_EXCLUSIVE } from "@/domains/dictionary/feature/config/skillExclusive.js";
+import { PITCHER_RECOMMEND } from "@/data/skill/PITCHER_RECOMMEND.js";
+
+export const DICTIONARY_PLAYER_CONFIG = {
+  hitter: {
+    requestType: "HITTER",
+    recommendData: HITTER_RECOMMEND,
+    skillExclusive: HITTER_SKILL_EXCLUSIVE,
+  },
+  pitcher: {
+    requestType: "PITCHER",
+    recommendData: PITCHER_RECOMMEND,
+    skillExclusive: null,
+  },
+}
