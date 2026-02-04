5cad3bef9529861d3e2a354520b719dc5fde56e6
Jae-Hong-Kim <newfly101@naver.com>
2026-02-04 18:18:40 +0900

    [page] 현재 등록된 선수(투수/타자) 기준으로 feature 분리 작업 추가 - 기존에 만들어둔 sample page 들은 삭제 예정

---
+++ web/src/domains/dictionary/feature/components/player/PlayerDictionaryView.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DICTIONARY_TAB_KEY, DictionaryNavTab } from "@/domains/dictionary/feature/config/DictionaryNavTab.js";
+import { useContentPageHeader } from "@/global/ui/contentPageHeader/index.js";
+import { requestPlayerSkillSet } from "@/domains/dictionary/store/index.js";
+import { ContentPageLayout } from "@/global/layout/contentPageLayout/index.js";
+import MetaHeader from "@/global/ui/metaHeader/MetaHeader.jsx";
+import CommonNavigationTab from "@/global/ui/navigation/tabs/CommonNavigationTab.jsx";
+import PlayerSkillDictionary from "@/domains/dictionary/feature/components/views/PlayerSkillDictionary.jsx";
+
+const PlayerDictionaryView = ({ config }) => {
+  const dispatch = useDispatch();
+  const { playerSkills } = useSelector((state) => state.dictionary);
+  const [tab, setTab] = useState(DictionaryNavTab[0].key);
+  const { moveTo } = useContentPageHeader();
+
+  useEffect(() => {
+    dispatch(requestPlayerSkillSet(config.requestType));
+  }, [dispatch]);
+
+  return (
+    <ContentPageLayout
+      header={
+        <MetaHeader
+          backLabel="백과사전 홈으로"
+          onBack={() => moveTo("/dictionary")}
+        />
+      }
+    >
+      <CommonNavigationTab tabs={DictionaryNavTab} onChange={setTab} activeKey={tab} />
+
+      {/*{tab === DICTIONARY_TAB_KEY.DESCRIPTION && (<SkillDescriptionSection />)}*/}
+      {tab === DICTIONARY_TAB_KEY.COMBINATION && config.type === "player" && (
+        <PlayerSkillDictionary skillData={playerSkills} recommendData={config.recommendData} skillExclusive={config.skillExclusive} />)}
+
+    </ContentPageLayout>
+  );
+};
+
+export default PlayerDictionaryView;
--- web/src/domains/dictionary/page/DictionaryPage.jsx
+++ web/src/domains/dictionary/page/DictionaryPage.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { DICTIONARY_HOME_META } from "@/meta/pages/dictionary.meta.js";
-import DictionaryViews from "@/domains/dictionary/feature/components/views/DictionaryViews.jsx";
 import { DICTIONARY_PLAYER_CONFIG } from "@/domains/dictionary/feature/config/player/dictionaryPlayerConfig.js";
+import PlayerDictionaryView from "@/domains/dictionary/feature/components/player/PlayerDictionaryView.jsx";
+import CoachDictionaryView from "@/domains/dictionary/feature/components/coach/CoachDictionaryView.jsx";
+import TeamDictionaryView from "@/domains/dictionary/feature/components/team/TeamDictionaryView.jsx";
 
 const DictionaryPage = () => {
   const { pathname } = useLocation();
 
   const type = pathname.split("/").pop();
   const meta = DICTIONARY_HOME_META.contexts[type];
-  const config = DICTIONARY_PLAYER_CONFIG[type];
 
   // 미구현 NavigationCard 실수로 open 시 변경
-  if (!meta || !config) {
+  if (!meta) {
     return <Navigate to="/dictionary" replace />;
   }
 
-  return <DictionaryViews config={config} />;
+  switch (type) {
+    case "hitter":
+    case "pitcher":
+    {
+      const config = DICTIONARY_PLAYER_CONFIG[type];
+      if (!config) return <Navigate to="/dictionary" replace />;
+      return <PlayerDictionaryView config={config} />;
+    }
+
+    case "coach":
+      return <CoachDictionaryView />;
+
+    case "team":
+      return <TeamDictionaryView />;
+
+    default:
+      return <Navigate to="/dictionary" replace />;
+  }
 };
 
 export default DictionaryPage;
