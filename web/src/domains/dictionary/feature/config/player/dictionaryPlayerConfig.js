2c3f698680f0bbff6ea146005387e2ec9f7b98d9
Jae-Hong-Kim <newfly101@naver.com>
2026-02-04 17:59:03 +0900

    [config] config  type 추가 - player  추후 coach, team 추가 시 분기 기점

--- web/src/domains/dictionary/feature/config/player/dictionaryPlayerConfig.js
+++ web/src/domains/dictionary/feature/config/player/dictionaryPlayerConfig.js
@@ -4,11 +4,13 @@ import { PITCHER_RECOMMEND } from "@/data/skill/PITCHER_RECOMMEND.js";
 
 export const DICTIONARY_PLAYER_CONFIG = {
   hitter: {
+    type: "player",
     requestType: "HITTER",
     recommendData: HITTER_RECOMMEND,
     skillExclusive: HITTER_SKILL_EXCLUSIVE,
   },
   pitcher: {
+    type: "player",
     requestType: "PITCHER",
     recommendData: PITCHER_RECOMMEND,
     skillExclusive: null,
--- web/src/domains/dictionary/page/DictionaryPage.jsx
+++ web/src/domains/dictionary/page/DictionaryPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, useLocation, useParams } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { DICTIONARY_HOME_META } from "@/meta/pages/dictionary.meta.js";
 import DictionaryViews from "@/domains/dictionary/feature/components/views/DictionaryViews.jsx";
 import { DICTIONARY_PLAYER_CONFIG } from "@/domains/dictionary/feature/config/player/dictionaryPlayerConfig.js";
@@ -12,7 +12,7 @@ const DictionaryPage = () => {
   const config = DICTIONARY_PLAYER_CONFIG[type];
 
   // 미구현 NavigationCard 실수로 open 시 변경
-  if (!meta) {
+  if (!meta || !config) {
     return <Navigate to="/dictionary" replace />;
   }
 
