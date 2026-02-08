2bba0ac1a21950433fc5b25817b664d519624ade
Jae-Hong-Kim <newfly101@naver.com>
2026-02-08 12:25:26 +0900

    [GENERIC] id 는 항상 Long 으로 받기 때문에 필요 없어서 제거

--- src/main/java/com/dawne/com2usbaseball/common/dto/OperationResponse.java
+++ src/main/java/com/dawne/com2usbaseball/common/dto/OperationResponse.java
@@ -1,15 +1,15 @@
 package com.dawne.com2usbaseball.common.dto;
 
-public record OperationResponse <M, T>(
+public record OperationResponse <M>(
         boolean success,
         M message,
-        T id
+        Long id
 ) {
-    public static <M,T> OperationResponse<M,T> success(M message, T id) {
+    public static <M> OperationResponse<M> success(M message, Long id) {
         return new OperationResponse<>(true, message, id);
     }
 
-    public static <M,T> OperationResponse<M,T> fail(M message) {
+    public static <M> OperationResponse<M> fail(M message) {
         return new OperationResponse<>(false, message, null);
     }
 }
