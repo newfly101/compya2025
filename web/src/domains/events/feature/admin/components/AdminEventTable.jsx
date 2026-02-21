937295e0c9ddcfb3e90edbb54ad2f559a4b16b06
Jae-Hong-Kim <newfly101@naver.com>
2026-02-22 04:55:48 +0900

    [refactor] [page] FE event 구조화 변경 - 이벤트 table 에 className 추가 REF : #11

--- web/src/domains/events/feature/admin/components/AdminEventTable.jsx
+++ web/src/domains/events/feature/admin/components/AdminEventTable.jsx
@@ -5,13 +5,13 @@ import EventTableBody from "@/domains/events/feature/admin/components/table/Even
 import EventEditModal from "@/domains/events/feature/admin/components/modal/EventEditModal.jsx";
 import EventCreateModal from "@/domains/events/feature/admin/components/modal/EventCreateModal.jsx";
 import AdminFilterBar from "@/global/layout/adminPageLayout/table/AdminFilterBar.jsx";
-import { useEventAdminFilter } from "@/domains/events/feature/admin/hooks/useEventAdminFilter.js";
-import { useEventListAdmin } from "@/domains/events/feature/admin/hooks/useEventListAdmin.js";
+import { useAdminEventTable } from "@/domains/events/feature/admin/hooks/useAdminEventTable.js";
+import { useAdminEventFilter } from "@/domains/events/feature/admin/hooks/useAdminEventFilter.js";
 
-const EventListAdmin = () => {
-  const { events, changeVisible } = useEventListAdmin();
-  const { filters, setFilters, filterUnits, filteredData: filteredEvents }
-    = useEventAdminFilter(events);
+const AdminEventTable = () => {
+  const { events, changeVisible } = useAdminEventTable();
+  const { filters, setFilters, filteredData: filteredEvents }
+    = useAdminEventFilter(events);
 
   const [open, setOpen] = useState(false);
   const [editEvent, setEditEvent] = useState(false);
@@ -23,8 +23,7 @@ const EventListAdmin = () => {
           title={"이벤트"}
           filters={filters}
           setFilters={setFilters}
-          filterUnits={filterUnits}
-          action={() => setOpen(true)}
+          onCreate={() => setOpen(true)}
         />}
         head={<EventTableHead />}
         tbody={
@@ -34,6 +33,7 @@ const EventListAdmin = () => {
             setEditEvent={setEditEvent}
           />
         }
+        tableClass="adminTableEvent"
       />
       {open && <EventCreateModal onClose={() => setOpen(false)} />}
       {editEvent && (<EventEditModal event={editEvent} onClose={() => setEditEvent(false)} />)}
@@ -41,4 +41,4 @@ const EventListAdmin = () => {
   );
 };
 
-export default EventListAdmin;
+export default AdminEventTable;
