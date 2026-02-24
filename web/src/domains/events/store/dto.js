c74419cb4754f09630f92e813f9415f305754cfd
Jae-Hong-Kim <newfly101@naver.com>
2026-02-25 04:31:17 +0900

    [refactor] [api] FE event 최적화 - 백엔드에 맞게 dto 수정 - 기존 불필요하게 나열 했던 DTO 삭제 REF : #11

--- web/src/domains/events/store/dto.js
+++ web/src/domains/events/store/dto.js
@@ -1,59 +1,9 @@
-/**
- * POST /events
- * @param {Object} state
- * @param {string} state.title
- * @param {string} state.eventSource
- * @param {string|Date} state.startAt
- * @param {string|Date} state.expireAt
- * @param {string} state.imageUrl
- * @param {string} state.externalLink
- * @param {boolean} state.visible
- *
- * @returns {{
- *   title: string,
- *   eventSource: string,
- *   startAt: string|Date,
- *   expireAt: string|Date,
- *   imageUrl: string,
- *   externalLink: string,
- *   visible: boolean
- * }}
- */
-export const createEventDTO = (state) => ({
+export const baseEventDTO = (state) => ({
   title: state.title,
-  eventSource: state.eventSource,
+  eventType: state.eventType,
   startAt: state.startAt,
   expireAt: state.expireAt,
   imageUrl: state.imageUrl,
   externalLink: state.externalLink,
   visible: state.visible,
-});
-
-/**
- * PATCH /events/{id}
- *
- * @param {Object} state
- * @param {string} state.title
- * @param {string} state.eventSource
- * @param {string|Date} state.startAt
- * @param {string|Date} state.expireAt
- * @param {string} state.imageUrl
- * @param {string} state.externalLink
- *
- * @returns {{
- *   title: string,
- *   eventSource: string,
- *   startAt: string|Date,
- *   expireAt: string|Date,
- *   imageUrl: string,
- *   externalLink: string
- * }}
- */
-export const updateEventDTO = (state) => ({
-  title: state.title,
-  eventSource: state.eventSource,
-  startAt: state.startAt,
-  expireAt: state.expireAt,
-  imageUrl: state.imageUrl,
-  externalLink: state.externalLink,
 });
