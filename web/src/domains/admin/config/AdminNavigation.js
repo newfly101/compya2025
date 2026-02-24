6e48eda53d947b4479a82881af2631f8a0d37360
Jae-Hong-Kim <newfly101@naver.com>
2026-02-25 06:15:58 +0900

    [refactor] [move] FE admin navigation - 이벤트 작업 완료로 인해 @ 삭제 REF : #11

--- web/src/domains/admin/config/AdminNavigation.js
+++ web/src/domains/admin/config/AdminNavigation.js
@@ -1,7 +1,7 @@
 export const AdminNavigation = [
   { url: "/admin", name: "대시보드@", end: true },
   { url: "/admin/content/notice", name: "공지 관리@" },
-  { url: "/admin/content/event", name: "이벤트 관리@" },
+  { url: "/admin/content/event", name: "이벤트 관리" },
   { url: "/admin/content/coupon", name: "쿠폰 관리" },
   { url: "/admin/users", name: "유저 관리@" },
   { url: "/admin/community", name: "게시판 관리@" },
