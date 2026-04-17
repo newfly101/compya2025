3511247fdea7e0de8d86ffd8bb65f8b888b9bb91
Jae-Hong-Kim <newfly101@naver.com>
2026-04-17 11:44:00 +0900

    [API] [coupon] coupon List 불러오는 API 수정 - FE 에서 filter 로 처리 하기 - BE 에서 cache data 로 넘어오기 때문에 따로 추가 수정하지 않음

--- web/src/domains/coupons/store/public/thunks.js
+++ web/src/domains/coupons/store/public/thunks.js
@@ -5,9 +5,12 @@ import { fetchGetUserCoupon } from "@/domains/coupons/store/public/api.js";
 export const requestGetUserCouponList = createAsyncThunk(
   COUPON_ACTIONS.GET_COUPON_LIST, async (_, { rejectWithValue }) => {
     try {
-      const { items } = await fetchGetUserCoupon();
+      const { data } = await fetchGetUserCoupon();
 
-      return [...items].sort((a, b) => b.id - a.id);
+      return [...data]
+        .filter(coupon => coupon.visible)
+        .sort((a, b) => b.id - a.id);
+
     } catch (error) {
       return rejectWithValue(error.message);
     }
