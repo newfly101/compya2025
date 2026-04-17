70663bf73772ef713c551916c4fb93e025525389
Jae-Hong-Kim <newfly101@naver.com>
2026-04-17 11:42:56 +0900

    [coupon] couponCard 디테일한 Action 버튼 추가 및 data 수정

--- web/src/domains/coupons/components/couponCard/CouponCard.jsx
+++ web/src/domains/coupons/components/couponCard/CouponCard.jsx
@@ -1,18 +1,27 @@
 // domains/coupon/components/CouponCard.jsx
 import styles from "./CouponCard.module.scss";
 
-const CouponCard = ({ coupon, children, isExpired = false }) => {
+const CouponCard = ({ coupon, showDetail = false, isExpired = false }) => {
+  const details = coupon.detail?.split("\n") ?? [];
+
+  const handleGoCoupon = () => {
+    if (isExpired) return;
+    window.open(`http://withhive.me/399/${coupon.couponCode}`, "_blank");
+  };
+
   return (
-    <article className={`${styles.couponCard} ${isExpired ? styles.expired : ''}`}>
+    <article className={`${styles.couponCard} ${isExpired ? styles.expired : ""}`}>
       <div className={styles.couponTop}>
-        <span className={styles.couponCode}>{coupon.code}</span>
-        <button className={`${styles.couponGoBtn} ${isExpired ? styles.expiredBtn : ""}`}>바로가기</button>
+        <span className={styles.couponCode}>{coupon.couponCode}</span>
+        <button className={`${styles.couponGoBtn} ${isExpired ? styles.expiredBtn : ""}`}
+                onClick={handleGoCoupon}>바로가기
+        </button>
       </div>
       <p className={styles.couponTitle}>{coupon.title}</p>
 
-      {children && (
+      {showDetail && coupon?.detail.length > 0 && (
         <div className={styles.couponBody}>
-          {children.map((item, idx) => (
+          {details.map((item, idx) => (
             <p key={idx}>{item}</p>
           ))}
         </div>
@@ -20,9 +29,9 @@ const CouponCard = ({ coupon, children, isExpired = false }) => {
 
       <p className={styles.couponExpire}>
         <span className={styles.expireDot}>⏱</span>
-        유효기간 {coupon.expiredAt}
+        유효기간 {coupon.expireAt}
       </p>
-      {children && (
+      {showDetail && (
         <p className={styles.couponExplain}>
           바로가기 버튼을 누르면 게임이 실행되며 쿠폰을 수령합니다.
         </p>
