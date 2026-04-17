012db128204ba1df9e787d090f2594901528fe24
Jae-Hong-Kim <newfly101@naver.com>
2026-04-17 11:54:54 +0900

    [리뉴얼] [delete] coupon 기존 PC 용 파일 삭제 REF: #26

--- web/src/domains/coupons/feature/admin/pages/AdminCouponListPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AdminCouponTable from "@/domains/coupons/feature/admin/components/AdminCouponTable.jsx";
-
-const AdminCouponListPage = () => {
-  return <AdminCouponTable />
-};
-
-export default AdminCouponListPage;
