081e8070c5eea2ab04c1bb8d174132c2cf2bc01b
Jae-Hong-Kim <newfly101@naver.com>
2026-04-17 19:26:48 +0900

    [리뉴얼] [delete] event 기존 PC 용 파일 삭제 REF : #28

--- web/src/domains/events/feature/admin/pages/AdminEventPage.jsx
+++
@@ -1,7 +0,0 @@
-import AdminEventTable from "@/domains/events/feature/admin/components/AdminEventTable.jsx";
-
-const AdminEventPage = () => {
-  return <AdminEventTable />
-};
-
-export default AdminEventPage;
--- web/src/domains/events/feature/public/pages/EventListPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EventListSection from "@/domains/events/feature/public/components/EventListSection.jsx";
-
-const EventListPage = () => {
-  return <EventListSection  />
-};
-
-export default EventListPage;
