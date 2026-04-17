afae69390a9b87395b7f2a33c6ad9d785048ff64
Jae-Hong-Kim <newfly101@naver.com>
2026-04-17 09:01:15 +0900

    [coupon] 공용 components 로 변경 - data 형태 수정 - Section에서 필요한 것만 뽑아서 쓸 수 있도록 변경

--- web/src/domains/home/components/section/coupon/CouponSection.jsx
+++ web/src/domains/home/components/section/coupon/CouponSection.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import styles from "./CouponSection.module.scss";
-import { MOCK_COUPONS } from "@/domains/home/config/MOCK_COUPONS.js";
+import { MOCK_COUPONS2 } from "@/domains/home/config/MOCK_COUPONS.js";
+import CouponCard from "@/domains/coupons/components/couponCard/CouponCard.jsx";
 
 const CouponSection = () => {
   return (
     <div className={styles.couponScroll}>
-      {MOCK_COUPONS.map((coupon) => (
-        <div key={coupon.id} className={styles.couponCard}>
-          <div className={styles.couponTop}>
-            <span className={styles.couponCode}>{coupon.code}</span>
-            <button className={styles.couponGoBtn}>바로가기</button>
-          </div>
-          <p className={styles.couponTitle}>{coupon.title}</p>
-          <p className={styles.couponExpire}>
-            {/*<span className={styles.expireDot}>●</span>*/}
-            유효기간 {coupon.expiredAt}
-          </p>
-        </div>
+
+      {MOCK_COUPONS2.map((coupon) => (
+        <CouponCard
+          key={coupon.id}
+          coupon={coupon}
+          // children={coupon.rewards}
+          isExpired={false}
+        />
       ))}
     </div>
   );
--- web/src/domains/home/config/MOCK_COUPONS.js
+++ web/src/domains/home/config/MOCK_COUPONS.js
@@ -4,3 +4,29 @@ export const MOCK_COUPONS = [
   { id: 2, code: "DREAMYDEBUT1ST", title: "GM드리미의 데뷔 쿠폰", expiredAt: "2026-05-31 23:59" },
   { id: 3, code: "DREAMYGO2ND", title: "GM드리미의 2차 트리플렛", expiredAt: "2026-05-31 23:59" },
 ];
+
+
+export const MOCK_COUPONS2 = [
+  {
+    id: 1,
+    code: "DREAMY602ND",
+    title: "GM드리미의 2차 쿠폰",
+    rewards: ["에픽 트리플랙 x1"],
+    expiredAt: "2026-05-31 23:59",
+    isExpired: false,
+  },
+  {
+    id: 4,
+    code: "NEWYEARCPB2026",
+    title: "설날 쿠폰",
+    rewards: [
+      "구단 선택 플래티넘 트리플렉 x1",
+      "고유능력 변경권 x3",
+      "리그 하드 볼 교환권 x2",
+      "리그모드 위원장 x20",
+      "스타 x1000",
+    ],
+    expiredAt: "2026-03-31 23:59",
+    isExpired: true,
+  },
+]
